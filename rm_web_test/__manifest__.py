2645f5c3b9cf183bb8480e7ed4cbd325376e5535
Roman Batavi <romanbatavi98@gmail.com>
2023-07-11 15:10:13 +0700

    [ADD]rm_web_test: kanban view

--- rm_web_test/__manifest__.py
+++ rm_web_test/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','portal'],
+    'depends': ['base','portal','website'],
 
     # always loaded
     'data': [
--- rm_web_test/controllers/portal.py
+++ rm_web_test/controllers/portal.py
@@ -6,9 +6,8 @@ from odoo import http
 class WebTestPortal(CustomerPortal):
     
     def _prepare_home_portal_values(self, counters):
-        
         ret = super(WebTestPortal, self)._prepare_home_portal_values(counters)
-        ret['employee_count'] = request.env['hr.employee'].sudo().search_count([])
+        ret['product_count'] = request.env['product.product'].sudo().search_count([])
         return ret
 
     # @http.route(['/my/tes_portal'], type='http')
@@ -18,7 +17,7 @@ class WebTestPortal(CustomerPortal):
     
     @http.route(['/my/tes_portal'], type='http', website=True)
     def tesportalview(self, **kw):
-        employee_obj = request.env['hr.employee']
-        employees = employee_obj.search([])
-        vals = {'employees':employees, 'page_name':'view_tes_portal'}
-        return request.render('rm_web_test.view_tes_portal', vals)
+        product_obj = request.env['product.product']
+        products = product_obj.search([])
+        vals = {'products':products, 'page_name':'view_tes_product'}
+        return request.render('rm_web_test.view_tes_product', vals)
