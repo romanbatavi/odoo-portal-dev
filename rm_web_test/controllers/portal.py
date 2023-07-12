4a1513e49ea41199fe3290d6ceb3108177d399f6
Roman Batavi <romanbatavi98@gmail.com>
2023-07-12 08:59:25 +0700

    [ADD]rm_web_test: adding new template for detail product

--- rm_web_test/controllers/portal.py
+++ rm_web_test/controllers/portal.py
@@ -10,14 +10,19 @@ class WebTestPortal(CustomerPortal):
         ret['product_count'] = request.env['product.product'].sudo().search_count([])
         return ret
 
-    # @http.route(['/my/tes_portal'], type='http')
+    # @http.route(['/my/tes_product'], type='http')
     # def tesportalview(self, **kw):
     #     print("HALO SEMUANYA")
     #     return "HALO SEMUANYA"
     
-    @http.route(['/my/tes_portal'], type='http', website=True)
-    def tesportalview(self, **kw):
+    @http.route(['/my/tes_product'], type='http', website=True)
+    def tes_product(self, **kw):
         product_obj = request.env['product.product']
         products = product_obj.search([])
         vals = {'products':products, 'page_name':'view_tes_product'}
         return request.render('rm_web_test.view_tes_product', vals)
+    
+    @http.route(['/my/tes_product/<model("product.product"):product_id>'], type='http', website=True)
+    def tes_product_form(self, product_id, **kw):
+        vals = {'products':product_id,  'page_name':'view_form_product'}
+        return request.render('rm_web_test.view_form_tes_product', vals)
